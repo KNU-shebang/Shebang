c1aabce0ba04c7a9bdb4e178a25c97f26c1c820a
seul lee <blacksangi14@naver.com>
2017-03-23 15:13:39 +0900

    oneroom/urls.py 생성
    방 정보 관리 및 댓글 작성 urls 구현

---
+++ shebang/oneroom/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+from wiki import views
+
+urlpatterns = [
+    # oneroom index 
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    
+    # 방 상세 페이지
+    url(r'^room/(?P<pk>\d+)/$', views.RoomDetailView.as_view(),
+        name='room'),
+    
+    # 방 신규 생성
+    url(r'^room/new/$', views.room_new, neme='room_new'),
+
+    # 방 정보 수정
+    url(r'^room/(?P<pk>\d+)/$', views.room_edit, name='room_edit'),
+
+    # 댓글 달기
+    url(r'^room/(?P<pk>\d+)/comment/new/$', views.comment_new, 
+        name='comment_new'),
+]
+
